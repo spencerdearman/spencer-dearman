672fe8a9976f8d8e91cf9565b205d0253f1c47e2
Spencer Dearman <dearmanspencer@gmail.com>
2025-12-16 19:19:18 -0600

    updating

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -74,39 +74,48 @@ const Explore = () => {
             </div>
           </div>
 
-          {/* Tile 4: Google Home */}
-          <div className="md:col-span-2 px-4 sm:px-12 py-4 md:p-2 relative">
-            <div
-              className={`tile-style aspect-square md:aspect-[2/1] relative ${
-                focusedTile === 'google-home' ? 'z-30' : ''
-              }`}
-            >
-              <div className="w-1/2 sm:w-1/6">
-                <SkillIcon
-                  imgSrc={'/images/google-home.svg'}
-                  label={'Google Home'}
-                />
-              </div>
-              <div
-                onMouseEnter={() => setFocusedTile('google-home')}
-                onMouseLeave={() => setFocusedTile(null)}
-              >
-                <ArrowButton href="https://home.google.com/welcome/" />
-              </div>
-            </div>
-            <div
-              className={`absolute -bottom-[40px] left-0 right-0 pt-2 text-center transition-opacity duration-600 ease-in-out ${
-                focusedTile === 'google-home'
-                  ? 'opacity-100 z-30'
-                  : 'opacity-0 pointer-events-none'
-              }`}
-            >
-              <p className="font-semibold text-gray-800">Google</p>
-              <p className="text-sm text-gray-500">
-                Software Engineering Internship
-              </p>
-            </div>
-          </div>
+{/* Tile 4: Google Home */}
+<div className="md:col-span-2 px-4 sm:px-12 py-4 md:p-2 relative group">
+  <div
+    className={`tile-style aspect-square md:aspect-[2/1] relative overflow-hidden ${
+      focusedTile === 'google-home' ? 'z-30' : ''
+    }`}
+  >
+    <div className="flex flex-col justify-start pt-20 md:justify-center md:pt-0 h-full md:pl-6 md:pr-72 items-center md:items-start text-center md:text-left relative z-20 pointer-events-none">
+      <p className="text-3xl font-bold text-gray-800 font-['Open_Sans']">
+        Google Nest Internship
+      </p>
+      <p className="text-base md:text-lg text-gray-500 font-['Open_Sans'] mt-2">
+        More information coming soon.
+      </p>
+    </div>
+
+    <img
+      src="/images/nest-cam.png"
+      alt="Nest Cam"
+      className="absolute -bottom-0 md:-right-8 w-[300px] md:w-[420px] object-contain z-10 opacity-90"
+    />
+    
+    <div
+      onMouseEnter={() => setFocusedTile('google-home')}
+      onMouseLeave={() => setFocusedTile(null)}
+    >
+      <ArrowButton href="https://home.google.com/welcome/" />
+    </div>
+  </div>
+  <div
+    className={`absolute -bottom-[40px] left-0 right-0 pt-2 text-center transition-opacity duration-600 ease-in-out ${
+      focusedTile === 'google-home'
+        ? 'opacity-100 z-30'
+        : 'opacity-0 pointer-events-none'
+    }`}
+  >
+    <p className="font-semibold text-gray-800">Google</p>
+    <p className="text-sm text-gray-500">
+      Software Engineering Internship
+    </p>
+  </div>
+</div>
 
           {/* Tile 5: Infleqtion */}
           <div className="px-4 sm:px-12 py-4 md:p-2 relative">
